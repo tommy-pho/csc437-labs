d557f37f4093da44b7ec5db394938c8303460233
Tommy Pho <tommypho@Tommys-MBP-4.lan>
2025-05-13 14:06:17 -0700

    using custom events for a better switch

--- packages/proto/public/js/main.js
+++ packages/proto/public/js/main.js
@@ -1,16 +1,40 @@
 const themeCheckbox = document.getElementById('theme-checkbox');
+const themeSwitchLabel = document.querySelector('.theme-switch');
 
-function applyTheme() {
-    if (themeCheckbox.checked) {
+if (themeSwitchLabel) {
+    themeSwitchLabel.addEventListener('change', function(event) {
+        event.stopPropagation(); 
+
+        const isChecked = event.target.checked;
+
+        const themeChangeEvent = new CustomEvent('theme:toggle', {
+            bubbles: true,
+            detail: { isDarkMode: isChecked }
+        });
+        event.target.dispatchEvent(themeChangeEvent);
+        console.log('Dispatched theme:toggle event:', { isDarkMode: isChecked });
+    });
+}
+
+document.body.addEventListener('theme:toggle', function(event) {
+    console.log('Caught theme:toggle event on body, detail:', event.detail);
+    if (event.detail && event.detail.isDarkMode) {
         document.body.classList.add('dark-mode');
     } else {
         document.body.classList.remove('dark-mode');
     }
+});
+
+function loadThemePreference() {
+    if (themeCheckbox && themeCheckbox.checked) {
+         document.body.classList.add('dark-mode');
+    } else {
+         document.body.classList.remove('dark-mode');
+    }
 }
 
-if (themeCheckbox) {
-    themeCheckbox.addEventListener('change', applyTheme);
-    applyTheme(); 
+if(themeCheckbox) {
+    loadThemePreference(); 
 }
 
-console.log('Theme switcher script loaded.'); 
+console.log('Theme switcher (custom event version) script loaded.');
