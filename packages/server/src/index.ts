79315d511bce9885eac0eccce4fa1e715260c96c
Tommy Pho <tommypho@Tommys-MBP-5.lan>
2025-05-27 20:04:05 -0700

    add express route to serve project data

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { connect } from "./services/mongo";
+import ProjectService from "./services/project-svc.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,17 @@ app.use(express.static(staticDir));
 
 app.get("/hello", (req: Request, res: Response) => {
   res.send("Hello, World from Express Server!");
+});
+
+app.get("/api/projects", (req: Request, res: Response) => {
+  ProjectService.index()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error fetching projects from database.");
+    });
 });
 
 app.listen(port, () => {
